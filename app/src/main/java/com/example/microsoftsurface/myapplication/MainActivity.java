790a90cdf381302fe5b5dbcf98326a4cd3aa6fc0
Surface\Microsoft Surface <iman.abrishami69@gmail.com>
2016-11-22 21:57:06 +0330

    Change app

--- app/src/main/java/com/example/microsoftsurface/myapplication/MainActivity.java
+++ app/src/main/java/com/example/microsoftsurface/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        Intent get=getIntent();
+        final String strFname=get.getStringExtra("Firstname");
+        final String strLname=get.getStringExtra("Lastname");
+        final String strEmail=get.getStringExtra("Email");
+        final String strPassword=get.getStringExtra("Password");
+        final String age1=get.getStringExtra("Age");
+        final int age=Integer.parseInt(get.getStringExtra("Age"));
+
 
         Button signup=(Button)findViewById(R.id.signup);
         signup.setOnClickListener(new View.OnClickListener() {
@@ -26,13 +37,36 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        Toast.makeText(MainActivity.this,strFname + strLname.toString() + strEmail.toString()+age1.toString() +strPassword.toString(),Toast.LENGTH_SHORT).show();
+
+
         Button signin=(Button)findViewById(R.id.signin);
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,WellcomePage.class);
-                        startActivity(intent);
-                finish();
+
+                EditText email= (EditText) findViewById(R.id.Email1);
+                String stremail=email.getText().toString();
+                EditText password= (EditText) findViewById(R.id.Password1);
+                String strpassword=password.getText().toString();
+
+                if(stremail.trim().equals("") || !stremail.equals(strEmail) || strpassword.trim().equals("")||
+                         (Integer.parseInt(strpassword.toString()))!=(Integer.parseInt(strPassword.toString())))
+                {
+                    Toast.makeText(MainActivity.this,R.string.Error1, Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    Toast.makeText(MainActivity.this,R.string.Successful, Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, WellcomePage.class);
+                    intent.putExtra("Firstname",strFname);
+                    intent.putExtra("Lastname",strLname);
+                    intent.putExtra("Email",strEmail);
+                    intent.putExtra("Age",age1);
+                    startActivity(intent);
+                    finish();
+                }
+                
             }
         });
     }
--- app/src/main/java/com/example/microsoftsurface/myapplication/WellcomePage.java
+++ app/src/main/java/com/example/microsoftsurface/myapplication/WellcomePage.java
@@ -1,8 +1,11 @@
 package com.example.microsoftsurface.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class WellcomePage extends AppCompatActivity {
 
@@ -11,5 +14,23 @@ public class WellcomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wellcome_page);
         getSupportActionBar().hide();
+
+        Intent get=getIntent();
+        final String strFname=get.getStringExtra("Firstname");
+        final String strLname=get.getStringExtra("Lastname");
+        final String strEmail=get.getStringExtra("Email");
+        final String strAge=get.getStringExtra("Age");
+
+        //Toast.makeText(this,strFname.toString()+ strLname.toString() + strEmail.toString()+String.valueOf(strAge),Toast.LENGTH_SHORT).show();
+
+        TextView Name= (TextView) findViewById(R.id.Yname);
+        TextView Email= (TextView) findViewById(R.id.Yemail);
+        TextView Age= (TextView) findViewById(R.id.Yage);
+
+        Name.setText(strFname.toString()+" "+strLname.toString());
+        Email.setText(strEmail.toString());
+        Age.setText(strAge.toString());
+
     }
+
 }
